551d39e1f72341dfeab736f31234706fb198bcac
Finnik723 <hydrosanek@google.com>
2020-08-02 00:35:39 +0300

    typo fix

--- src/Battlescape/BattlescapeGenerator.cpp
+++ src/Battlescape/BattlescapeGenerator.cpp
@@ -660,7 +660,7 @@ void BattlescapeGenerator::run()
 				{
 					throw Exception(ruleDeploy->getType() +  " is not defined in rulesets!");
 				}
-				tries++;
+				++tries;
 			}
 		}
 		else
@@ -669,8 +669,6 @@ void BattlescapeGenerator::run()
 		}
 	}
 	
-
-
 	_save->setTurnLimit(ruleDeploy->getTurnLimit());
 	_save->setChronoTrigger(ruleDeploy->getChronoTrigger());
 	_save->setCheatTurn(ruleDeploy->getCheatTurn());
