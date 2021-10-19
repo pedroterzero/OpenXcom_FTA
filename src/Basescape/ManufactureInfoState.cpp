ee7e17a5642cd7cc47c6a29b62e2857f8589ee47
Finnik <45336117+Finnik723@users.noreply.github.com>
2021-10-20 01:10:05 +0300

    hide autosell for fta #107

--- src/Basescape/ManufactureInfoState.cpp
+++ src/Basescape/ManufactureInfoState.cpp
@@ -206,7 +206,7 @@ void ManufactureInfoState::buildUi()
 		_base->addProduction(_production);
 	}
 	_btnSell->setPressed(_production->getSellItems());
-	_btnSell->setVisible(_production->getRules()->canAutoSell());
+	_btnSell->setVisible(_production->getRules()->canAutoSell() && !_game->getMod()->getIsFTAGame());
 	initProfitInfo();
 	setAssignedEngineer();
 
