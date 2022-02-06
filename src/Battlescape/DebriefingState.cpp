narical/openxcom-accuracy

commit 13b8d6f74ee734300ca6c4c8e24f293867ec442c

Prevent wrong loot display in corner cases
When the craft is lost and some xcom equipment was sent back
to base before the mission (by starting conditions),
it would show as loot (because it cannot be reequipped to craft).

src/Battlescape/DebriefingState.cpp
@@ -409,7 +409,7 @@ void DebriefingState::init()
 	}
 
 	// compare stuff from after and before recovery
-	if (_base)
+	if (_base && _showSellButton)
 	{
 		int row = 0;
 		ItemContainer *origBaseItems = _game->getSavedGame()->getSavedBattle()->getBaseStorageItems();
@@ -1992,6 +1992,10 @@ void DebriefingState::prepareDebriefing()
 	if (craft)
 	{
 		reequipCraft(base, craft, true);
+	}
+	else
+	{
+		hideSellTransferButtons();
 	}
 
 	if (target == "STR_BASE")

pedroterzero/OpenXcom_FTA

commit 13afd35138e4220be539c520734e69436740b422

fix for #127
was broken in https://github.com/MeridianOXC/OpenXcom/commit/13b8d6f74ee734300ca6c4c8e24f293867ec442c
brutal fix, that does not solve the initial problem

src/Battlescape/DebriefingState.cpp
@@ -457,7 +457,7 @@ void DebriefingState::init()
 	}
 
 	// compare stuff from after and before recovery
-	if (_base && _showSellButton)
+	if (_base)
 	{
 		int row = 0;
 		ItemContainer *origBaseItems = _game->getSavedGame()->getSavedBattle()->getBaseStorageItems();